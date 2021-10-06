153e36423fec59778f3d9aaaf70ee5e6a680032a
VRJVOID <sanketshirsath226@gmail.com>
2021-10-06 17:18:21 +0530

    Added Rough Test

--- 05-First Java Program/Main.java
+++ 05-First Java Program/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-        System.out.println("hel");
+       Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
     }
 }    
