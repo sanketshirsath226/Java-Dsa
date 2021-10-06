6bb56a03f2eac8e5ec36184ffdf9ed5d36799a40
VRJVOID <sanketshirsath226@gmail.com>
2021-10-06 17:44:31 +0530

    Added Rough Test

--- 05-First Java Program/Main.java
+++ 05-First Java Program/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
-        System.out.println(input.nextLine());
+        int a = 254;
+        byte b = (byte) a;//Max value is
     }
-}    
+}
