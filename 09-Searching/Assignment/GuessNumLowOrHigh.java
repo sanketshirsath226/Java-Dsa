7742ad9837d68975315d84ef1289d3de76c77154
sanketshirsath226@gmail.com <sanketshirsath226@gmail.com>
2022-12-12 00:35:47 +0530

    Successfully Completed Guess Number with Time Complexity : 0ms and Space Complexity : 39.1 mb

---
+++ 09-Searching/Assignment/GuessNumLowOrHigh.java
@@ -0,0 +1,65 @@
+/*
+374. Guess Number Higher or Lower
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+Example 1:
+
+Input: n = 10, pick = 6
+Output: 6
+Example 2:
+
+Input: n = 1, pick = 1
+Output: 1
+Example 3:
+
+Input: n = 2, pick = 1
+Output: 1
+ 
+Constraints:
+
+1 <= n <= 231 - 1
+1 <= pick <= n
+
+*/
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int start = 1;
+        int end = n;
+        while(start<=end){
+            int mid = start + (end - start) / 2;
+            int guessNo = guess(mid);
+            if(guessNo == 0){
+                return mid;
+            }
+            if(guessNo == -1){
+                end = mid - 1;
+            }
+            if(guessNo == 1){
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
+/* Runtime : 0 ms      Memory: 39.1 MB */
