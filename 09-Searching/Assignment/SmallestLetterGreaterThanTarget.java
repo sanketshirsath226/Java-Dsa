0279d42b0d8ce0efab4b32d9659d19709b036434
sanketshirsath226@gmail.com <sanketshirsath226@gmail.com>
2022-12-12 15:45:19 +0530

    Successfully Completed with Smaller Letter Greater Than Target with Runtime: 1 ms Memory Usage: 48.2 MB

--- 09-Searching/Assignment/SmallestLetterGreaterThanTarget.java
+++ 09-Searching/Assignment/SmallestLetterGreaterThanTarget.java
@@ -1,14 +1,6 @@
 /* 
  744. Find Smallest Letter Greater Than Target
-Easy
 
-2440
-
-1926
-
-Add to List
-
-Share
 You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
 
 Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
