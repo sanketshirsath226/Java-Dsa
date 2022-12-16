153ef3678c6af98c90db661836899291a7aa0ad1
sanketshirsath226@gmail.com <sanketshirsath226@gmail.com>
2022-12-16 14:03:50 +0530

    Successfully Completed the Peak Index in a Mountain Array with Runtime: 0 ms, Memory Usage: 59.5 MB

---
+++ 09-Searching/Assignment/PeakIndexinaMountainArray.java
@@ -0,0 +1,52 @@
+/*
+ 852. Peak Index in a Mountain Array
+An array arr a mountain if the following properties hold:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+You must solve it in O(log(arr.length)) time complexity.
+
+ 
+
+Example 1:
+
+Input: arr = [0,1,0]
+Output: 1
+Example 2:
+
+Input: arr = [0,2,1,0]
+Output: 1
+Example 3:
+
+Input: arr = [0,10,5,2]
+Output: 1
+ 
+
+Constraints:
+
+3 <= arr.length <= 105
+0 <= arr[i] <= 106
+arr is guaranteed to be a mountain array.
+ */
+public class PeakIndexinaMountainArray {
+    public static void main(String[] args){
+        System.out.println(peakIndexInMountainArray(new int[]{0,10,5,2}));
+    }
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <=end){
+            int mid = start + (end - start) /2;
+            if(arr[mid] > arr[mid+1]){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return end+1;
+    }
+}
