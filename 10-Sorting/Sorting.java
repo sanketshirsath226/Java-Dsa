9b5e7eac08f462b3f47dc3bb0ff8a65abb038bd1
sanketshirsath226@gmail.com <sanketshirsath226@gmail.com>
2022-12-24 11:00:17 +0530

    Successfully Completed with Insertion Sort

--- 10-Sorting/Sorting.java
+++ 10-Sorting/Sorting.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class Sorting {
     public static void main(String[] args){
         int[] arr = new int[]{1,4,3,2,0};
-        selection(arr);
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
     }
     /*
@@ -56,6 +56,22 @@ public class Sorting {
         }
     }
 
+    /*
+     Insertion Sort
+     parameters : int - 2-D Arr
+     return : none
+    */
+    public static void insertion(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j>0;j--){
+                if(arr[j-1]>arr[j]){
+                    swap(arr,j-1,j);
+                }else{
+                    break;
+                }
+            }
+        }
+    }
     /*
         Max Element Index Position
         parameters : int - 2-D Arr, int Start , int End
